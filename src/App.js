875166f4aab71a4ef07deec76c2cb4ec2ebbb1eb
Cyci25 <c2muriithi@gmail.com>
2019-02-17 10:58:29 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +19,9 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <h1>My name is Cynthia muriithi</h1>
+        <h2>And I love chicken</h2>
       </div>
     );
   }
