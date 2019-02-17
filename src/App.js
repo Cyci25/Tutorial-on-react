506fe92a75ed4ed9bdfdf9d47c21dd420157d56d
Cyci25 <c2muriithi@gmail.com>
2019-02-17 11:56:31 +0300

    implement a REST API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Table, Button } from 'reactstrap';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    books: []
+  }
+  componentWillMount() {
+    axios.get('http://localhost:3000/books').then((response) => {
+      this.setState({
+        books: response.data
+      })
+    });
+  }
   render() {
+    let books = this.state.books.map((book) => {
+      return (
+        <tr key={book.id}>
+          <td>{book.id}</td>
+          <td>{book.title}</td>
+          <td>{book.rating}</td>
+          <td>
+            <button class="btn btn-success" size="sm">Edit</button>
+            <button class="btn btn-danger" size="sm">Delete</button>
+          </td>
+        </tr>
+      )
+    });
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Rating</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books}
+            {/* <tr>
+              <td>1</td>
+              <td>Book title</td>
+              <td>2.5</td>
+              <td>
+                <button class="btn btn-success" size="sm">Edit</button>
+                <button class="btn btn-danger" size="sm">Delete</button>
+              </td>
+            </tr> */}
+          </tbody>
+        </Table>
         <h1>My name is Cynthia muriithi</h1>
         <h2>And I love chicken</h2>
       </div>
